12edc2e732406814664282ea2901c72b2a480920
shift19 <verebtamas@outlook.com>
2024-06-22 17:13:37 +0200

    update template to the latest changes

--- app/routes/webhooks/route.tsx
+++ app/routes/webhooks/route.tsx
@@ -4,19 +4,26 @@ import db from '~/db.server';
 import { authenticate } from '~/shopify.server';
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-    const { topic, shop, session, admin } = await authenticate.webhook(request);
+    const { topic, shop, session, admin, webhookId } = await authenticate.webhook(request);
 
     if (!admin) {
         // The admin context isn't returned if the webhook fired after a shop was uninstalled.
         throw new Response();
     }
 
+    console.log('Webhook received:', webhookId, topic, shop);
+
+    // The topics handled here should be declared in the shopify.app.toml.
+    // More info: https://shopify.dev/docs/apps/build/cli-for-apps/app-configuration
     switch (topic) {
         case 'APP_UNINSTALLED':
             if (session) {
                 await db.session.deleteMany({ where: { shop } });
             }
 
+            break;
+        case 'PRODUCTS_CREATE':
+            console.log('Product created');
             break;
         case 'CUSTOMERS_DATA_REQUEST':
         case 'CUSTOMERS_REDACT':
--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -1,30 +1,21 @@
 import '@shopify/shopify-app-remix/adapters/node';
 import { restResources } from '@shopify/shopify-api/rest/admin/2024-04';
-import { AppDistribution, DeliveryMethod, LATEST_API_VERSION, shopifyApp } from '@shopify/shopify-app-remix/server';
+import { ApiVersion, AppDistribution, shopifyApp } from '@shopify/shopify-app-remix/server';
 import { PrismaSessionStorage } from '@shopify/shopify-app-session-storage-prisma';
 import prisma from '~/db.server';
+
+export const apiVersion = ApiVersion.April24;
 
 const shopify = shopifyApp({
     apiKey: process.env.SHOPIFY_API_KEY,
     apiSecretKey: process.env.SHOPIFY_API_SECRET || '',
-    apiVersion: LATEST_API_VERSION,
+    apiVersion,
     scopes: process.env.SCOPES?.split(','),
     appUrl: process.env.SHOPIFY_APP_URL || '',
     authPathPrefix: '/auth',
     sessionStorage: new PrismaSessionStorage(prisma),
     distribution: AppDistribution.AppStore,
     restResources,
-    webhooks: {
-        APP_UNINSTALLED: {
-            deliveryMethod: DeliveryMethod.Http,
-            callbackUrl: '/webhooks',
-        },
-    },
-    hooks: {
-        afterAuth: async ({ session }) => {
-            await shopify.registerWebhooks({ session });
-        },
-    },
     future: {
         unstable_newEmbeddedAuthStrategy: true,
     },
@@ -32,10 +23,8 @@ const shopify = shopifyApp({
 });
 
 export default shopify;
-export const apiVersion = LATEST_API_VERSION;
 export const addDocumentResponseHeaders = shopify.addDocumentResponseHeaders;
 export const authenticate = shopify.authenticate;
 export const unauthenticated = shopify.unauthenticated;
 export const login = shopify.login;
-export const registerWebhooks = shopify.registerWebhooks;
 export const sessionStorage = shopify.sessionStorage;
